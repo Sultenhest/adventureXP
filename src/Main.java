1b76c19330c06fc4d3bc968cda43d57488012093
Lasse-A <lass731d@stud.kea.dk>
2017-03-16 15:23:35 +0100

    ændret i udskrift

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+
+
         System.out.println("Hej!");
-        System.out.println("sdfs");
+
         System.out.println("nej!");
     }
 }
