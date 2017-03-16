52d66e391f29697151bcdd80f52e877daf4daa3e
Lasse-A <lass731d@stud.kea.dk>
2017-03-16 15:08:20 +0100

    Tilføjet system out linie

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hej!");
         System.out.println("sdfs");
+        System.out.println("Nu er jeg med!");
     }
 }
