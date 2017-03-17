2073fdbdad1299daa360ba97f1c73744df76e857
YaMehdi313 <ahma1003@stud.kea.dk>
2017-03-17 09:18:15 +0100

    Initial groundwork on editing activites uploaded

---
+++ src/ActivityInteraction.java
@@ -0,0 +1,17 @@
+/**
+ * Created by YaMehdi313 on 17-03-2017.
+ */
+public class ActivityInteraction
+{
+    public void viewSelected()
+    {}
+
+    public void editSelected()
+    {
+    }
+
+    public void deleteSelected()
+    {}
+
+
+}
