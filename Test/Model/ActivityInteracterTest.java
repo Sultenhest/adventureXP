4a32bd99142e258834f1db8716652a13c6c9193b
ShinyTimes <andr4666@stud.kea.dk>
2017-03-17 11:51:13 +0100

    Added additional tests and methods to ActivityInteracter

--- Test/Model/ActivityInteracterTest.java
+++ Test/Model/ActivityInteracterTest.java
@@ -18,20 +18,62 @@ public class ActivityInteracterTest
     }
 
     @Test
-    public void saveTest() throws Exception
+    public void saveTestCorrectSQLStatement() throws Exception
     {
         ActivityInteracter AI = new ActivityInteracter();
         Activity activity = new Activity(1, 18, "Helicopter Flying", "The name LITERALLY says it all: You fly a fucking helicopter!");
 
         assertEquals("INSERT INTO activity VALUES 1, 'Helicopter Flying', 18, 0", AI.save(activity));
+
+        DatabaseConnect.getStatement().executeQuery(AI.save(activity));
+
+        saveTestCorrectDatabaseRead(activity, AI);
     }
 
     @Test
-    public void editTest() throws Exception
+    public void saveTestCorrectDatabaseRead(Activity activity, ActivityInteracter AI) throws Exception
+    {
+        assertEquals("1, Helicopter Flying, 18, 0",
+                "" + DatabaseConnect.getStatement().executeQuery("SELECT * FROM activity WHERE activity.act_id = " + activity.getID()));
+    }
+
+    @Test
+    public void editIDTestCorrectSQLStatement() throws Exception
     {
         ActivityInteracter AI = new ActivityInteracter();
         Activity activity = new Activity(1, 18, "Helicopter Flying", "The name LITERALLY says it all: You fly a fucking helicopter!");
 
-        assertEquals("INSERT INTO activity (act_id) VALUES 1 WHERE activity.act_id = " + activity.getID(), AI.edit(activity));
+        assertEquals("INSERT INTO activity (act_id) VALUES 2 WHERE activity.act_id = " + activity.getID(), AI.editID(activity));
+
+        DatabaseConnect.getStatement().executeQuery(AI.editID(activity));
+
+        editIDTestCorrectDatabaseRead(activity, AI);
+    }
+
+    @Test
+    public void editIDTestCorrectDatabaseRead(Activity activity, ActivityInteracter AI) throws Exception
+    {
+        assertEquals("2, Helicopter Flying, 18, 0",
+                "" + DatabaseConnect.getStatement().executeQuery("SELECT * FROM activity WHERE activity.act_id = " + activity.getID()));
+    }
+
+    @Test
+    public void editNameTestCorrectSQLStatement() throws Exception
+    {
+        ActivityInteracter AI = new ActivityInteracter();
+        Activity activity = new Activity(1, 18, "Helicopter Flying", "The name LITERALLY says it all: You fly a fucking helicopter!");
+
+        assertEquals("INSERT INTO activity (act_name) VALUES 'Helicopter Driving' WHERE activity.act_id = " + activity.getID(), AI.editName(activity));
+
+        DatabaseConnect.getStatement().executeQuery(AI.editName(activity));
+
+        editNameTestCorrentDatabaseRead(activity, AI);
+    }
+
+    @Test
+    public void editNameTestCorrentDatabaseRead(Activity activity, ActivityInteracter AI) throws Exception
+    {
+        assertEquals("2, Helicopter Driving, 18, 0",
+                "" + DatabaseConnect.getStatement().executeQuery("SELECT * FROM activity WHERE activity.act_id = " + activity.getID()));
     }
 }
